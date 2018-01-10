630f63f978d9fc18b05919ef1071675c49c0a6ec
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-01-09 22:41:23 -1000

    working with height>1 texture

--- example/objects.js
+++ example/objects.js
@@ -1,8 +1,12 @@
 var regl = require('regl')({
-  extensions: [ 'oes_standard_derivatives', 'oes_texture_float' ]
+  extensions: [
+    'oes_standard_derivatives',
+    'oes_texture_float',
+    'oes_element_index_uint'
+  ]
 })
 var camera = require('regl-camera')(regl,
-  { distance: 5 })
+  { distance: 120, phi: -0.6 })
 var ch = require('conway-hart')
 var mat4 = require('gl-mat4')
 var vec3 = require('gl-vec3')
@@ -10,22 +14,20 @@ var glsl = require('glslify')
 
 var formulas = [
   'dO', 'aO', 'tC', 'tO', 'eO', 'taO', 'sO',
-  'O', 'jO', 'kO', 'dtO', 'oO', 'mO', 'gO'
+  'O', 'jO', 'kO', 'dtO', 'oO', 'mO', 'gO',
+  'D'
 ]
-var N = formulas.length
+var N = 800
+var mwidth = 64
+var mheight = 50
+
 var matrices = new Float32Array(16*N)
-var offsets = new Float32Array(3*N)
 var rotations = new Float32Array(4*N)
 
 var tmpv = new Float32Array(3)
 var tmpm = new Float32Array(16)
 
 for (var i = 0; i < N; i++) {
-  var r = i/N * 20
-  var theta = i/N*2*Math.PI
-  offsets[i*3+0] = Math.cos(theta)*r
-  offsets[i*3+1] = Math.sin(theta)*r
-  offsets[i*3+2] = 0
   vec3.random(tmpv, 1.0)
   rotations[i*4+0] = tmpv[0]
   rotations[i*4+1] = tmpv[1]
@@ -34,41 +36,31 @@ for (var i = 0; i < N; i++) {
 }
 
 function update (time) {
+  var tau = Math.PI*2
+  var n = formulas.length
   for (var i = 0; i < N; i++) {
-    //var m = matrices.subarray(i*16,i*16+16)
+    var m = matrices.subarray(i*16,i*16+16)
+    var theta = i/n*2*Math.PI/3 + i/N
+    var r = Math.pow(i/n,0.5) * 10
+    mat4.identity(m)
+    tmpv[0] = (1-Math.pow(i/n,0.4))*2
+      + Math.sin(theta/2+time*tau*0.5)*0.5 + 10
+    tmpv[1] = Math.sin(theta)*r
+    tmpv[2] = Math.cos(theta)*r
+    mat4.translate(m, m, tmpv)
     tmpv[0] = rotations[i*4+0]
     tmpv[1] = rotations[i*4+1]
     tmpv[2] = rotations[i*4+2]
     var rspeed = rotations[i*4+3]
-    mat4.identity(tmpm)
-    mat4.rotate(tmpm, tmpm, time*rspeed, tmpv)
-    copy(matrices, i, tmpm)
+      + theta/tau*0.2
+    mat4.rotate(m, m, time*rspeed+i, tmpv)
   }
 }
 
-function copy (dst, i, src) {
-  dst[i*16+0] = src[0]
-  dst[i*16+1] = src[1]
-  dst[i*16+2] = src[2]
-  dst[i*16+3] = src[3]
-  dst[i*16+4] = src[4]
-  dst[i*16+5] = src[5]
-  dst[i*16+6] = src[6]
-  dst[i*16+7] = src[7]
-  dst[i*16+8] = src[8]
-  dst[i*16+9] = src[9]
-  dst[i*16+10] = src[10]
-  dst[i*16+11] = src[11]
-  dst[i*16+12] = src[12]
-  dst[i*16+13] = src[13]
-  dst[i*16+14] = src[14]
-  dst[i*16+15] = src[15]
-}
-
 var mesh = { positions: [], cells: [], ids: [] }
-for (var i = 0; i < formulas.length; i++) {
+for (var i = 0; i < N; i++) {
   var offset = mesh.positions.length
-  var m = ch(formulas[i])
+  var m = ch(formulas[i%formulas.length])
   for (var j = 0; j < m.positions.length; j++) {
     mesh.positions.push(m.positions[j])
     mesh.ids.push(i)
@@ -85,9 +77,10 @@ for (var i = 0; i < formulas.length; i++) {
   }
 }
 
+
 var mtex = regl.texture({
-  width: matrices.length/4,
-  height: 1,
+  width: mwidth,
+  height: mheight,
   format: 'rgba',
   wrapS: 'clamp',
   wrapT: 'clamp',
@@ -99,10 +92,12 @@ var draw = regl({
     #extension GL_OES_standard_derivatives : enable
     precision highp float;
     #pragma glslify: faceNormal = require('glsl-face-normal')
+    #pragma glslify: hsl2rgb = require('glsl-hsl2rgb')
     varying vec3 vpos;
     void main () {
       vec3 fnorm = faceNormal(vpos);
-      gl_FragColor = vec4(fnorm*0.5+0.5,1);
+      vec3 color = hsl2rgb((fnorm*0.5+0.5)*vec3(1,0.8,0.5)+vec3(0,0.2,0.4));
+      gl_FragColor = vec4(color,1);
     }
   `,
   vert: glsl`
@@ -113,13 +108,15 @@ var draw = regl({
     attribute float id;
     varying vec3 vpos;
     void main () {
-      float w = ${(N*4-1).toFixed(1)};
-      float ix = id*4.0;
+      float w = ${(mwidth-1).toFixed(1)};
+      float h = ${(mheight-1).toFixed(1)};
+      float tx = mod(id*4.0,w+1.0);
+      float ty = floor(id*4.0/w);
       mat4 model = mat4(
-        texture2D(mtex,vec2((ix+0.0)/w,0.0)),
-        texture2D(mtex,vec2((ix+1.0)/w,0.0)),
-        texture2D(mtex,vec2((ix+2.0)/w,0.0)),
-        texture2D(mtex,vec2((ix+3.0)/w,0.0))
+        texture2D(mtex,vec2((tx+0.0)/w,ty/h)),
+        texture2D(mtex,vec2((tx+1.0)/w,ty/h)),
+        texture2D(mtex,vec2((tx+2.0)/w,ty/h)),
+        texture2D(mtex,vec2((tx+3.0)/w,ty/h))
       );
       vpos = position;
       gl_Position = projection * view * model * vec4(position,1);
