694d752d2df187688893c20ee5509dd176d47807
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-01-21 16:31:18 -1000

    fix for width=0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (N) {
   var sqN = Math.sqrt(N*4)
   var width = Math.ceil(sqN)
   width = width + (4-(width%4))%4
-  var height = Math.ceil(N*4/width)
+  var height = width > 0 ? Math.ceil(N*4/width) : 0
   return {
     width: width,
     height: height,
