6fb55c921dce74a02303820ae6e3e8c7c2ca142c
lostsourcecode <42322205+lostsourcecode@users.noreply.github.com>
2018-08-12 19:19:21 +0200

    Update index.glsl
    Added an offset to texel coordinates so that they target the 'center' of the texel.

--- index.glsl
+++ index.glsl
@@ -1,13 +1,14 @@
 mat4 read_matrix (sampler2D tex, float i, vec2 size) {
-  float w = size.x-1.0;
-  float h = size.y-1.0;
-  float tx = mod(i*4.0+0.5/size.x,size.x);
-  float ty = floor(i*4.0/w);
+  // Apply an offset so that the coordinate targets the center of the texel, otherwise the wrong texel might be read causing incorrect matrices
+  float offset = 0.5; 
+  float tx = mod(i * 4.0, size.x) + offset;
+  float ty = floor(i * 4.0 / size.x) + offset;
+  
   return mat4(
-    texture2D(tex,vec2((tx+0.0)/w,ty/h)),
-    texture2D(tex,vec2((tx+1.0)/w,ty/h)),
-    texture2D(tex,vec2((tx+2.0)/w,ty/h)),
-    texture2D(tex,vec2((tx+3.0)/w,ty/h))
+    texture2D(tex, vec2((tx + 0.0) / size.x, ty / size.y)),
+    texture2D(tex, vec2((tx + 1.0) / size.x, ty / size.y)),
+    texture2D(tex, vec2((tx + 2.0) / size.x, ty / size.y)),
+    texture2D(tex, vec2((tx + 3.0) / size.x, ty / size.y))
   );
 }
 #pragma glslify: export(read_matrix)
